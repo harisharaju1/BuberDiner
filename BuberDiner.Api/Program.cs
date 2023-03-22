108ee5719b7e86914ff89bd81710702dec0444d1
Harish Raju <harisharaju1@gmail.com>
2023-03-22 09:35:04 +0530

    Global error handling using middleware and custom details factory implementation

--- BuberDiner.Api/Program.cs
+++ BuberDiner.Api/Program.cs
@@ -1,5 +1,7 @@
+using BuberDiner.Api.Errors;
 using BuberDiner.Application;
 using BuberDiner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
